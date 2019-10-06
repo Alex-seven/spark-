78e299a47a130bd8123c934797dba1760e7b3a6c
alex-seven <alexisbusy@163.com>
2019-10-06 10:20:01 +0800

    提交文件

---
+++ TrafficProject/src/dao/impl/AreaDaoImpl.java
@@ -0,0 +1,34 @@
+package com.bjsxt.spark.dao.impl;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bjsxt.spark.dao.IAreaDao;
+import com.bjsxt.spark.domain.Area;
+import com.bjsxt.spark.domain.Task;
+import com.bjsxt.spark.jdbc.JDBCHelper;
+import com.bjsxt.spark.jdbc.JDBCHelper.QueryCallback;
+
+public class AreaDaoImpl implements IAreaDao {
+
+	public List<Area> findAreaInfo() {
+		final List<Area> areas = new ArrayList<Area>();
+		
+		String sql = "SELECT * FROM area_info";
+		
+		JDBCHelper jdbcHelper = JDBCHelper.getInstance();
+		jdbcHelper.executeQuery(sql, null, new QueryCallback() {
+			
+			public void process(ResultSet rs) throws Exception {
+				if(rs.next()) {
+					String areaId = rs.getString(1);
+					String areaName = rs.getString(2);
+					areas.add(new Area(areaId, areaName));
+				}
+			}
+		});
+		return areas;
+	}
+
+}
